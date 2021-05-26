07b070704e5ea7a644d24e6ac9252fefc6cda78a
RVaishnavi-999 <vaishnavi.r@cs.christuniversity.in>
2021-05-26 20:57:28 +0530

    Create Color to gray.py
    Converting Color Image to
    Grayscale Image.

---
+++ Color to gray.py
@@ -0,0 +1,9 @@
+import cv2
+image = cv2.imread('v.jpg')
+#converting image into grayscale image
+greyImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#naming the grayscale image as gray_image.png
+cv2.imwrite('gray_image.png', greyImage)
+cv2.imshow('Grey_image', greyImage)
+cv2.waitKey(0) 
+cv2.destroyAllWindows()
