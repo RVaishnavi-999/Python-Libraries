1a36b15212baf0f6d2636844c1c831af473714c5
Vaishnavi R <vkekuda@gmail.com>
2021-05-26 21:00:53 +0530

    Program 1

--- ImageReadWriteShow.py
+++ ImageReadWriteShow.py
@@ -3,4 +3,4 @@ img = cv2.imread('v.jpg')            #reading image from local directory
 cv2.imwrite('renamed.jpg',img)       #writing image in different name or renaming image
 cv2.imshow('show',img)               #display image, show will be printed on window
 cv2.waitKey(0)                       #infinite time 
-cv2.destroyAllWindows()              #close all windows
+cv2.destroyAllWindows()              #close all windows.
